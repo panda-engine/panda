3e061b9ccf817ea9a58f413a108cee32b0d914b3
maye174 <96584640+maye174@users.noreply.github.com>
2023-07-27 11:49:16 +0800

    fix args for log fn

--- src/log/test.cpp
+++ src/log/test.cpp
@@ -1,8 +1,10 @@
 #include "log.h"
 
 int main(){
+    int a = 0;
+    char s[] = "hello %s %d\n";
     set_log(all);
-    panda_log.debug("hello %s\n", "world");
+    panda_log.debug(s, "world", a);
     set_log_c(all);
     panda_log_c.debug("123");
 }
