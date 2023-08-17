0a039dd85df8f55dd0f89a60ebe8e6c76e532cfa
maye174 <96584640+maye174@users.noreply.github.com>
2023-08-17 20:14:53 +0800

    update obj

--- src/scene/inc/obj/obj.h
+++ src/scene/inc/obj/obj.h
@@ -11,6 +11,7 @@ extern "C" {
 
 typedef struct {
     char *name;
+    uint64_t id;
     int type;
     int is_enable;
     void *ptr;
@@ -25,20 +26,23 @@ typedef struct {
     int len;
 } obj_t;
 
+// obj fn
 obj_t *panda_new_obj(pmem *pm);
+void panda_set_obj_name(obj_t *obj, const char *name);
 void panda_free_obj(pmem *pm, obj_t *obj);
 
-void panda_set_obj_name(pmem *pm, obj_t *obj, const char *name);
-void panda_free_obj_name(pmem *pm, obj_t *obj);
 void panda_set_obj_id(obj_t *obj, const uint64_t id);
 
-void panda_set_component_name(pmem *pm, component_t *component,
-                              const char *name);
-void panda_free_component_name(pmem *pm, component_t *component);
+// component fn
+component_t *panda_new_component(pmem *pm);
+void panda_set_component_name(component_t *component, const char *name);
+void panda_free_component(pmem *pm, component_t *component);
 
+void panda_set_component_id(component_t *component, const uint64_t id);
+
+//
 void panda_obj_component_add(pmem *pm, obj_t *obj, component_t *cp);
-void panda_obj_component_free_all(pmem *pm, obj_t *obj);
-component_t *panda_obj_component_find(obj_t *obj, const int type);
+void panda_obj_component_free(pmem *pm, obj_t *obj);
 
 #ifdef __cplusplus
 }
--- src/scene/obj/obj.c
+++ src/scene/obj/obj.c
@@ -21,7 +21,119 @@ static void obj_component_add_mi_malloc(obj_t *obj, component_t *cp) {
     obj->array[obj->len++] = *cp;
 }
 
-void obj_component_add(pmem *pm, obj_t *obj, component_t *cp) {
+static void obj_component_mi_free(obj_t *obj) {
+    mi_free(obj->array);
+    obj->cap = 0;
+    obj->len = 0;
+}
+
+obj_t *panda_new_obj(pmem *pm) {
+
+    obj_t *r = NULL;
+
+    if (!pm) {
+        r = mi_malloc(sizeof(obj_t));
+    } else {
+        r = pm->alloc(sizeof(obj_t), pm->_ptr);
+    }
+
+    if (!r)
+        return NULL;
+
+    r->array = NULL;
+    r->cap = 0;
+    r->len = 0;
+    r->id = 0;
+    r->name = NULL;
+
+    return r;
+}
+
+void panda_set_obj_name(obj_t *obj, const char *name) {
+
+    if (!obj)
+        return;
+
+    if (obj->name)
+        mi_free(obj->name);
+
+    obj->name = mi_strdup(name);
+}
+
+void panda_free_obj(pmem *pm, obj_t *obj) {
+
+    mi_free(obj->name);
+
+    if (!pm) {
+        mi_free(obj);
+        return;
+    }
+
+    pm->free(obj, pm->_ptr);
+}
+
+void panda_set_obj_id(obj_t *obj, const uint64_t id) {
+
+    if (!obj)
+        return;
+
+    obj->id = id;
+}
+
+component_t *panda_new_component(pmem *pm) {
+
+    component_t *r = NULL;
+    if (!pm) {
+        r = mi_malloc(sizeof(component_t));
+    } else {
+        r = pm->alloc(sizeof(component_t), pm->_ptr);
+    }
+
+    if (!r)
+        return NULL;
+
+    r->name = NULL;
+    r->id = 0;
+    r->type = 0;
+    r->is_enable = 0;
+    r->ptr = NULL;
+    r->fn = NULL;
+
+    return r;
+}
+
+void panda_set_component_name(component_t *component, const char *name) {
+
+    if (!component)
+        return;
+
+    if (component->name)
+        mi_free(component->name);
+
+    component->name = mi_strdup(name);
+}
+
+void panda_free_component(pmem *pm, component_t *component) {
+
+    mi_free(component->name);
+
+    if (!pm) {
+        mi_free(component);
+        return;
+    }
+
+    pm->free(component, pm->_ptr);
+}
+
+void panda_set_component_id(component_t *component, const uint64_t id) {
+
+    if (!component)
+        return;
+
+    component->id = id;
+}
+
+void panda_obj_component_add(pmem *pm, obj_t *obj, component_t *cp) {
 
     if (!obj || !cp) {
         clog_error("obj_component_add: arg is null point!");
@@ -51,14 +163,17 @@ void obj_component_add(pmem *pm, obj_t *obj, component_t *cp) {
     obj->array[obj->len++] = *cp;
 }
 
-void obj_component_free(pmem *pm, obj_t *obj) {
+void panda_obj_component_free(pmem *pm, obj_t *obj) {
+
     if (!obj)
         return;
 
     if (!pm) {
+        obj_component_mi_free(obj);
+        return;
     }
 
-    while (obj->len > 0) {
-        component_t *p = &obj->array[--obj->len];
-    }
+    pm->free(obj->array, pm->_ptr);
+    obj->cap = 0;
+    obj->len = 0;
 }
